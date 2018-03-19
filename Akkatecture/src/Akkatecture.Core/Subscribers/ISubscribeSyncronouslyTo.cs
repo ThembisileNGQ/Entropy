ea3179c261b840fd95c1e863cfa9afb47b6aaa03
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-03-19 18:18:54 +0100

    files

---
+++ Akkatecture/src/Akkatecture.Core/Subscribers/ISubscribeSyncronouslyTo.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using Akkatecture.Aggregates;
+using Akkatecture.Core;
+
+namespace Akkatecture.Subscribers
+{
+    public interface ISubscribeSynchronousTo<TAggregate, in TIdentity, in TEvent>
+        where TAggregate : IAggregateRoot<TIdentity>
+        where TIdentity : IIdentity
+        where TEvent : IAggregateEvent<TAggregate, TIdentity>
+    {
+        void Handle(IDomainEvent<TAggregate, TIdentity, TEvent> domainEvent);
+    }
+}
