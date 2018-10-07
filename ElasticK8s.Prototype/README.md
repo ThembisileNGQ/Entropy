0b001e29e07fd5506a496738bb7290dbf2c21b0a
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-10-07 16:19:37 +0200

    some things

---
+++ ElasticK8s.Prototype/README.md
@@ -0,0 +1,6 @@
+# ElasticK8s.Prototype
+
+Prototyping the use of [KubernetesClient](https://github.com/kubernetes-client/csharp) to see how one can scale from within an application
+
+
+Currently a WIP, nothing to see here..
---
+++ ElasticK8s.Prototype/src/ElasticK8s.Prototype.Application/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using k8s;
+using k8s.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace ElasticK8s.Prototype.Application
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            IKubernetes client = new Kubernetes(config);
+            Console.WriteLine("Starting Request!");
+            
+            var deployments = await client.ListNamespacedDeploymentAsync("default");
+
+            foreach (var deployment in deployments.Items)
+            {
+                Console.WriteLine(deployment.Metadata.Name);
+            }
+            
+            if (deployments.Items.Count == 0)
+            {
+                Console.WriteLine("Empty!");
+            }
+
+            var a = await  client.ReadNamespacedDeploymentScaleAsync("nodejs-deployment","default");
+
+            var patch = new JsonPatchDocument<V1Scale>();
+
+            patch.Replace(p => p.Spec.Replicas, 4);
+
+            var result = await client.PatchNamespacedDeploymentScaleAsync(new V1Patch(patch), "nodejs-deployment", "default");
+        }
+    }
+}
