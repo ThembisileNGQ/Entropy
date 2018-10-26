4c5ffcfc53d19eb7db01f021d5bf4a4b7e2461ec
Unknown <lutando.ngqakaza@recordunion.com>
2018-10-26 08:40:15 +0200

    Playtime begins

--- Akkatecture.ResumableProjections/src/ReadApi/Startup.cs
+++ Akkatecture.ResumableProjections/src/ReadApi/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,8 +7,6 @@ namespace ReadApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
