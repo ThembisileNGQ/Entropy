e6aa03322463823e07e6ace11cb822b836372625
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-11-07 17:42:59 +0100

    yeah thats right dont use this code anyone!

---
+++ EventJournalPlayground/_k8s/README.md
@@ -0,0 +1,3 @@
+# EventJournalPlayground K8s files
+
+Do not use these in their current deployment type in production, these are just quick and easy k8s deployments for some backing stores
--- EventJournalPlayground/src/SimpleApplication/Program.cs
+++ EventJournalPlayground/src/SimpleApplication/Program.cs
@@ -13,30 +13,50 @@ namespace SimpleApplication
 {
     public class Program
     {
-        
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            
             //Create actor system
-            var system = ActorSystem.Create("useraccount-example",Config.Postgres);
+            var system = ActorSystem.Create("useraccount-example",Config.EventStore);
 
             //Create supervising aggregate manager for UserAccount aggregate root actors
             var aggregateManager = system.ActorOf(Props.Create(() => new UserAccountAggregateManager()));
 
             //Build create user account aggregate command
-            var aggregateId = UserAccountId.New;
-            var createUserAccountCommand = new CreateUserAccountCommand(aggregateId, "foo bar");
+            var aggregateId = UserAccountId.With("useraccount-b1e48522-0c58-4051-a4ba-01d9665ad35f");
+            //var createUserAccountCommand = new CreateUserAccountCommand(aggregateId, "foo bar");
             
             //Send command, this is equivalent to command.publish() in other cqrs frameworks
-            aggregateManager.Tell(createUserAccountCommand);
+            //aggregateManager.Tell(createUserAccountCommand);
 
-            var changeNameCommand = new UserAccountChangeNameCommand(aggregateId, "foo bar baz");
-            aggregateManager.Tell(changeNameCommand);
+            while (true)
+            {
+                var newName = GetRandomString();
+                var changeNameCommand = new UserAccountChangeNameCommand(aggregateId, newName);
+                aggregateManager.Tell(changeNameCommand);
+                await Task.Delay(1000);
+            }
+            
                         
             //block end of program
             Console.WriteLine("done");
             Console.ReadLine();
+        }
+
+        public static string GetRandomString()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             
+            var random = new Random();
+            var stringChars = new char[random.Next(3,12)];
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            return finalString;
         }
     }
 }
