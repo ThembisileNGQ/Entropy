502f93a4ef87a44ac3c2da5e4da872b625792bd0
Lutando Ngqakaza <lutando@ngqakaza.com>
2018-11-20 21:37:20 +0100

    fin

--- Akkatecture.ResumableProjections/src/ReadApi/CarsController.cs
+++ Akkatecture.ResumableProjections/src/ReadApi/CarsController.cs
@@ -1,9 +1,17 @@
+using System;
+using System.Threading.Tasks;
+using Domain.Model.Car;
+using Domain.Model.Car.Commands;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ReadApi
 {
     public class CarsController : Controller
     {
-        
+        [HttpGet("cars/{id:Guid}")]
+        public async Task<IActionResult>  GetCar([FromRoute] Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
