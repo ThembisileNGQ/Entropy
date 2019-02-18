1857e8c3f980258927714e4c22485b57bbbe31b6
Lutando Ngqakaza <lutando@ngqakaza.com>
2019-02-18 08:17:39 +0100

    wip

--- Akkatecture.ResumableProjections/src/ReadApi/CarProjector.cs
+++ Akkatecture.ResumableProjections/src/ReadApi/CarProjector.cs
@@ -1,4 +1,6 @@
-﻿using Akka.Actor;
+﻿using System.ComponentModel.Design.Serialization;
+using System.Reflection.Metadata.Ecma335;
+using Akka.Actor;
 
 namespace ReadApi
 {
@@ -6,5 +8,26 @@ namespace ReadApi
     {
         private static string _schema = "projection";
         private static string _table = "cars";
+        private readonly string _version;
+
+        public CarProjector(ProjectorSettings settings)
+        {
+            Self.Tell(BeginStreaming.Instance);   
+        }
+    }
+
+    public class BeginStreaming
+    {
+        public static BeginStreaming Instance => new BeginStreaming();
+    }
+    public class ProjectorSettings
+    {
+        public string Version { get; }
+
+        public ProjectorSettings(
+            string version)
+        {
+            Version = version;
+        }
     }
 }
